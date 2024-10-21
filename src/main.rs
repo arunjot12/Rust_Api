fccc20b68b54bc7bf697307dd2ef34d70240b7d1
ArunJot <arunjot@antiersolutions.com>
2024-10-21 17:33:35 +0530

    chore: fetched the current blockchain block number

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,36 @@ use jsonrpsee::ws_client::WsClientBuilder;
 
 #[tokio::main]
 async fn main() {
-    get_blockchain_name().await;
+    // get_blockchain_name().await;
+    get_blocknumber().await;
 }
 
-async fn get_blockchain_name() -> String {
+// async fn get_blockchain_name() -> String {
+//     // Create a WebSocket client and connect to the node
+//     let client = WsClientBuilder::default().build("ws://52.38.225.247:9944").await.expect("REASON");
+//     // Call the `system_chain` method to get the blockchain name
+//     let chain_name: String = client
+//     .request("system_chain", jsonrpsee::core::params::ArrayParams::new())
+//     .await
+//     .expect("Failed to retrieve the chain name");
+
+//     println!("Connected to chain: {}", chain_name);
+
+//     chain_name
+// }
+
+async fn get_blocknumber() -> u64 {
     // Create a WebSocket client and connect to the node
     let client = WsClientBuilder::default().build("ws://52.38.225.247:9944").await.expect("REASON");
     // Call the `system_chain` method to get the blockchain name
-    let chain_name: String = client
-    .request("system_chain", jsonrpsee::core::params::ArrayParams::new())
+    let block_number_hex: String = client
+    .request("eth_blockNumber",jsonrpsee::core::params::ArrayParams::new())
     .await
     .expect("Failed to retrieve the chain name");
 
-    println!("Connected to chain: {}", chain_name);
-
-    chain_name
+    let block_number = u64::from_str_radix(block_number_hex.trim_start_matches("0x"), 16)
+    .expect("Failed to convert block number to decimal");
+     
+    println!("Current block number: {}", block_number);
+    block_number
 }
