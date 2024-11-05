fbecd4b303383694cf6a5ae5989d54acbd5fa770
ArunJot <arunjot@antiersolutions.com>
2024-11-05 18:23:46 +0530

    chore: added the time interval for fetching the blocks continuously

--- src/firechain.rs
+++ src/firechain.rs
@@ -1,9 +1,6 @@
 use jsonrpsee::core::client::ClientT;
 use jsonrpsee::ws_client::WsClientBuilder;
-use substrate_api_client::{ Api, GetChainInfo };
-use substrate_api_client::rpc::JsonrpseeClient;
-use substrate_api_client::GetStorage;
-use substrate_api_client::ac_primitives::DefaultRuntimeConfig;
+
 #[allow(unused)]
 
 // async fn get_blockchain_name() -> String {
@@ -20,26 +17,27 @@ use substrate_api_client::ac_primitives::DefaultRuntimeConfig;
 //     chain_name
 // }
 
-// pub async fn get_blocknumber() -> u64 {
-//     // Create a WebSocket client and connect to the node
-//     let client = WsClientBuilder::default().build("ws://52.38.225.247:9944").await.expect("REASON");
-//     // Call the `system_chain` method to get the blockchain name
-//     let block_number_hex: String = client
-//     .request("eth_blockNumber",jsonrpsee::core::params::ArrayParams::new())
-//     .await
-//     .expect("Failed to retrieve the chain name");
-
-//     let block_number = u64::from_str_radix(block_number_hex.trim_start_matches("0x"), 16)
-//     .expect("Failed to convert block number to decimal");
-
-//     println!("Current block on 5irechain number: {}", block_number);
-//     block_number
-// }
-pub async fn totat_issuance() {
+pub async fn get_blocknumber() -> u64 {
     // Create a WebSocket client and connect to the node
-    let client = JsonrpseeClient::new("ws://52.38.225.247:9944").await.expect("REASON");
-    let api = Api::<DefaultRuntimeConfig, _>::new(client).await.unwrap();
-    let storage = api.get_genesis_block();
-    let balance: Option<u128> = api.get_storage::<u128>("Balances", "TotalIssuance", None).await.unwrap();
-    println!("Total Issuance is {:?}",balance);
+    let client = WsClientBuilder::default().build("ws://52.38.225.247:9944").await.expect("REASON");
+    // Call the `system_chain` method to get the blockchain name
+    let block_number_hex: String = client
+    .request("eth_blockNumber",jsonrpsee::core::params::ArrayParams::new())
+    .await
+    .expect("Failed to retrieve the chain name");
+
+    let block_number = u64::from_str_radix(block_number_hex.trim_start_matches("0x"), 16)
+    .expect("Failed to convert block number to decimal");
+
+    println!("Current block on 5irechain number: {}", block_number);
+    block_number
 }
+
+// pub async fn totat_issuance() {
+//     // Create a WebSocket client and connect to the node
+//     let client = JsonrpseeClient::new("ws://52.38.225.247:9944").await.expect("REASON");
+//     let api = Api::<DefaultRuntimeConfig, _>::new(client).await.unwrap();
+//     let storage = api.get_genesis_block();
+//     let balance: Option<u128> = api.get_storage::<u128>("Balances", "TotalIssuance", None).await.unwrap();
+//     println!("Total Issuance is {:?}",balance);
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 mod binance;
 pub mod firechain;
-use crate::binance::*;
 use crate::firechain::*;
+use tokio::time;
+use std::time::Duration;
 
 /// 5irechain
 #[tokio::main]
 async fn main() {
     // 5irechain
-    totat_issuance().await;
+    let mut interval = time::interval(Duration::from_secs(3));
+
+    // Continuously process each tick
+    loop {
+        // Wait for the next interval tick
+        interval.tick().await;
+
+        // Execute your asynchronous task (get_blocknumber in this case)
+        get_blocknumber().await;
+    }
 
     // Binance 
-
     // let url = "https://api.binance.com/api/v1/ticker/24hr";
     // let num = get_node(url);
     // dbg!(num);
