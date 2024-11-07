08e1b2c6a46dec0a2625e5130bdb29ff5cb104a6
ArunJot <arunjot@antiersolutions.com>
2024-11-07 11:28:00 +0530

    chore: refactored the code and added the dependency of diesel

--- src/firechain.rs
+++ src/firechain.rs
@@ -1,43 +1,40 @@
 use jsonrpsee::core::client::ClientT;
 use jsonrpsee::ws_client::WsClientBuilder;
+use substrate_api_client::{
+    Api,
+    GetStorage,
+    rpc::JsonrpseeClient,
+    ac_primitives::DefaultRuntimeConfig,
+};
 
-#[allow(unused)]
-
-// async fn get_blockchain_name() -> String {
-//     // Create a WebSocket client and connect to the node
-//     let client = WsClientBuilder::default().build("ws://52.38.225.247:9944").await.expect("REASON");
-//     // Call the `system_chain` method to get the blockchain name
-//     let chain_name: String = client
-//     .request("system_chain", jsonrpsee::core::params::ArrayParams::new())
-//     .await
-//     .expect("Failed to retrieve the chain name");
-
-//     println!("Connected to chain: {}", chain_name);
-
-//     chain_name
-// }
+pub async fn get_blockchain_name() -> String {
+    let client = WsClientBuilder::default().build("ws://52.38.225.247:9944").await.expect("REASON");
+    let chain_name: String = client
+        .request("system_chain", jsonrpsee::core::params::ArrayParams::new()).await
+        .expect("Failed to retrieve the chain name");
+    chain_name
+}
 
 pub async fn get_blocknumber() -> u64 {
     // Create a WebSocket client and connect to the node
     let client = WsClientBuilder::default().build("ws://52.38.225.247:9944").await.expect("REASON");
     // Call the `system_chain` method to get the blockchain name
     let block_number_hex: String = client
-    .request("eth_blockNumber",jsonrpsee::core::params::ArrayParams::new())
-    .await
-    .expect("Failed to retrieve the chain name");
-
-    let block_number = u64::from_str_radix(block_number_hex.trim_start_matches("0x"), 16)
-    .expect("Failed to convert block number to decimal");
-
+        .request("eth_blockNumber", jsonrpsee::core::params::ArrayParams::new()).await
+        .expect("Failed to retrieve the chain name");
+    let block_number = u64
+        ::from_str_radix(block_number_hex.trim_start_matches("0x"), 16)
+        .expect("Failed to convert block number to decimal");
     println!("Current block on 5irechain number: {}", block_number);
     block_number
 }
 
-// pub async fn totat_issuance() {
-//     // Create a WebSocket client and connect to the node
-//     let client = JsonrpseeClient::new("ws://52.38.225.247:9944").await.expect("REASON");
-//     let api = Api::<DefaultRuntimeConfig, _>::new(client).await.unwrap();
-//     let storage = api.get_genesis_block();
-//     let balance: Option<u128> = api.get_storage::<u128>("Balances", "TotalIssuance", None).await.unwrap();
-//     println!("Total Issuance is {:?}",balance);
-// }
+pub async fn total_issuance() -> Option<u128> {
+    // Create a WebSocket client and connect to the node
+    let client = JsonrpseeClient::new("ws://52.38.225.247:9944").await.expect("REASON");
+    let api = Api::<DefaultRuntimeConfig, _>::new(client).await.unwrap();
+    let balance: Option<u128> = api
+        .get_storage::<u128>("Balances", "TotalIssuance", None).await
+        .unwrap();
+    balance
+}
--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,23 @@ use std::time::Duration;
 /// 5irechain
 #[tokio::main]
 async fn main() {
+    // Binance 
+    // let url = "https://api.binance.com/api/v1/ticker/24hr";
+    // let num = get_node(url);
+    // dbg!(num);
+
     // 5irechain
+    let blockchain_name: String = get_blockchain_name().await;
+    let total_issuance: u128 = total_issuance().await.unwrap();
+    println!("Blockchain chain: {}", blockchain_name);
+    println!("Total issuance of {} blockchain is {:?}",blockchain_name,total_issuance);
     let mut interval = time::interval(Duration::from_secs(3));
 
     // Continuously process each tick
     loop {
         // Wait for the next interval tick
         interval.tick().await;
-
         // Execute your asynchronous task (get_blocknumber in this case)
         get_blocknumber().await;
     }
-
-    // Binance 
-    // let url = "https://api.binance.com/api/v1/ticker/24hr";
-    // let num = get_node(url);
-    // dbg!(num);
 }
